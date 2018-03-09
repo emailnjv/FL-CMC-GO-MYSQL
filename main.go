b56e23562d9df9768627da9005eaecea899511a0
Nick <emailnjv@gmail.com>
2018-03-09 09:52:22 -0500

    progress checkpoint

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/yhat/scrape"
@@ -25,22 +26,24 @@ var (
 
 type Currency struct {
 	Name              string
-	Pair              string
-	Volume            int
-	Price             int
-	CirculatingSupply int
-	oneHour           int
-	oneDay            int
-	oneWeek           int
+	Symbol            string
+	MarketCap         float64
+	Price             float64
+	CirculatingSupply float64
+	Volume            float64
+	oneHour           float64
+	oneDay            float64
+	oneWeek           float64
 }
 
 type Exchange struct {
+	ExchangeName    string
 	Currency        string
 	Pair            string
-	VolumeCur       int
-	Price           int
-	VolumePercent   int
-	UpdatedRecently int
+	VolumeCur       float64
+	Price           float64
+	VolumePercent   float64
+	UpdatedRecently bool
 }
 
 type UrlArrStruct struct {
@@ -67,12 +70,30 @@ func InsertExchange(exch Exchange) {
 	Price := exch.Price
 	VolumePercent := exch.VolumePercent
 	UpdatedRecently := exch.UpdatedRecently
-	insForm, err := db.Prepare("INSERT INTO exchange(TimeScraped=NOW(), Currency=?, Pair=?, VolumeCur=?, Price=?, VolumePercent=?, UpdatedRecently=?)")
+	insForm, err := db.Prepare("INSERT INTO exchanges(TimeScraped, ExchangeTitle, Currency, Pair, VolumeCur, Price, VolumePercent, UpdatedRecently) VALUES(?,?,?,?,?,?,?,?)")
 	if err != nil {
 		panic(err.Error())
 	}
-	insForm.Exec(Currency, Pair, VolumeCur, Price, VolumePercent, UpdatedRecently)
-	log.Println(Currency, Pair, VolumeCur, Price, VolumePercent, UpdatedRecently)
+	insForm.Exec(time.Now(), exch.ExchangeName, Currency, Pair, VolumeCur, Price, VolumePercent, UpdatedRecently)
+
+	defer db.Close()
+}
+func InsertCurrency(curr Currency) {
+	db := dbConn()
+	name := curr.Name
+	symbol := curr.Symbol
+	VolumeCur := curr.Volume
+	Price := curr.Price
+	CirculatingSupply := curr.CirculatingSupply
+	oneHour := curr.oneHour
+	oneDay := curr.oneDay
+	oneWeek := curr.oneWeek
+
+	insForm, err := db.Prepare("INSERT INTO all_currencies(TimeScraped, Currency, Symbol, Volume, Price, CirculatingSupply, oneHour, oneDay,oneWeek) VALUES(?,?,?,?,?,?,?,?,?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	insForm.Exec(time.Now(), name, symbol, VolumeCur, Price, CirculatingSupply, oneHour, oneDay, oneWeek)
 
 	defer db.Close()
 }
@@ -128,6 +149,7 @@ func resultNodeExchangeGen(in <-chan *html.Node) bool {
 	var wg sync.WaitGroup
 	for root := range in {
 		wg.Add(1)
+
 		rowMatcher := func(n *html.Node) bool {
 			if n.DataAtom == atom.Tr && n != nil && scrape.Attr(n.Parent.Parent, "id") == "exchange-markets" {
 				return n.Parent.DataAtom == atom.Tbody
@@ -135,24 +157,63 @@ func resultNodeExchangeGen(in <-chan *html.Node) bool {
 			return false
 		}
 
+		exchangeMatcher := func(n *html.Node) bool {
+			if n.DataAtom == atom.H1 {
+				return true
+			}
+			return false
+		}
+
 		var results = scrape.FindAll(root, rowMatcher)
+		var headResults, _ = scrape.Find(root, exchangeMatcher)
+		exchangeTitle := scrape.Text(headResults)
 
 		var wg1 sync.WaitGroup
 		wg1.Add(len(results))
 		// ********
-		for _, result := range results {
+		for _, resultz := range results {
+			defer wg1.Done()
 
-			fmt.Println(scrape.Text(result))
-			out := make(chan *html.Node)
-			go func(n *html.Node) {
-				defer wg1.Done()
-				if strings.Contains(scrape.Text(n), "Sponsored P.when") {
-					preString := strings.SplitAfter(scrape.Text(n), "? Leave ad feedback ")
-					out <- preString[1]
-				} else {
-					out <- scrape.Text(n)
-				}
-			}(result)
+			arr := strings.Split(scrape.Text(resultz), " ")
+
+			var updateMatcherResult bool
+			if arr[len(arr)-1] == "Recently" {
+				updateMatcherResult = true
+
+			}
+			if arr[len(arr)-1] != "Recently" {
+				updateMatcherResult = false
+
+			}
+
+			arrr1 := arr[len(arr)-5][1:]
+			arrr12 := strings.Replace(arrr1, ",", "", -1)
+			var arrr13, err1 = strconv.ParseFloat(arrr12, 64)
+			if err1 != nil {
+				fmt.Println(err1, arrr13)
+			}
+
+			arrr2 := arr[len(arr)-4][1:]
+			arrr22 := strings.Replace(arrr2, ",", "", -1)
+			var arrr23, err2 = strconv.ParseFloat(arrr22, 64)
+			if err2 != nil {
+				fmt.Println(err2)
+				fmt.Println("*********************")
+
+			}
+
+			arr3 := arr[len(arr)-3][1:]
+			arr32 := strings.Replace(arr3, ",", "", -1)
+			var arrr33, err3 = strconv.ParseFloat(arr32, 64)
+			if err3 != nil {
+				fmt.Println(err3)
+				fmt.Println("&&&&&&&&&&&&&&&&")
+			}
+			var arrr4 string
+			arrr4 = strings.Join(arr[1:len(arr)-6], " ")
+
+			InsertExchange(Exchange{exchangeTitle, arrr4, arr[len(arr)-6], arrr13, arrr23, arrr33, updateMatcherResult})
+
 		}
 		go func() {
 			wg1.Wait()
@@ -162,9 +223,115 @@ func resultNodeExchangeGen(in <-chan *html.Node) bool {
 	}
 	go func() {
 		wg.Wait()
-		close(out)
 	}()
-	return out
+	return true
+
+}
+func resultNodeCurrencyGen(in <-chan *html.Node) bool {
+	var wg sync.WaitGroup
+	for root := range in {
+		wg.Add(1)
+
+		rowMatcherQ := func(n *html.Node) bool {
+			if n.DataAtom == atom.Tr && n != nil && scrape.Attr(n.Parent.Parent, "id") == "currencies-all" {
+				return n.Parent.DataAtom == atom.Tbody
+			}
+			return false
+		}
+
+		var results = scrape.FindAll(root, rowMatcherQ)
+
+		var wg1 sync.WaitGroup
+		wg1.Add(len(results))
+		// ********
+		for _, resultz := range results {
+			defer wg1.Done()
+
+			arr := strings.Split(scrape.Text(resultz), " ")
+			fmt.Println(arr)
+
+			currSymbol := arr[1]
+
+			// arr3 := arr[len(arr)-3][1:]
+			// arr32 := strings.Replace(arr3, ",", "", -1)
+			// var arrr33, err3 = strconv.ParseFloat(arr32, 64)
+			// if err3 != nil {
+			// 	fmt.Println(err3)
+			// }
+			var newArr []string
+			if arr[len(arr)-5] == "*" {
+				var slice1 []string
+				slice1 = arr[:len(arr)-6]
+				var slice2 []string
+				slice2 = arr[len(arr)-4:]
+				slice1 = append(slice1, slice2...)
+				newArr = slice1
+			}
+			// modifiedNameString := strings.Replace(arr, ",", "", -1)
+			var currName string
+			currName = strings.Join(newArr[1:len(newArr)-8], " ")
+
+			marketc := arr[len(arr)-7][1:]
+			marketcWTH := strings.Replace(marketc, ",", "", -1)
+			var parsedMC, err2 = strconv.ParseFloat(marketcWTH, 64)
+			if err2 != nil {
+				fmt.Println(err2)
+			}
+
+			price := arr[len(arr)-6][1:]
+			priceWTH := strings.Replace(price, ",", "", -1)
+			var parsedPrice, err3 = strconv.ParseFloat(priceWTH, 64)
+			if err3 != nil {
+				fmt.Println(err3)
+			}
+			volume := arr[len(arr)-4][1:]
+			volumeWTH := strings.Replace(volume, ",", "", -1)
+			var volS, err34 = strconv.ParseFloat(volumeWTH, 64)
+			if err34 != nil {
+				fmt.Println(err34)
+			}
+			supply := arr[len(arr)-5]
+			supplyWTH := strings.Replace(supply, ",", "", -1)
+			var parsupply, err35 = strconv.ParseFloat(supplyWTH, 64)
+			if err35 != nil {
+				fmt.Println(err35)
+			}
+			hour := arr[len(arr)-3]
+			hourWTH := strings.Replace(hour, "%", "", -1)
+			var onehe, err325 = strconv.ParseFloat(hourWTH, 64)
+			var oneh = onehe / 100
+			if err325 != nil {
+				fmt.Println(err325)
+			}
+			day := arr[len(arr)-2]
+			dayWTH := strings.Replace(day, "%", "", -1)
+			var onede, err3225 = strconv.ParseFloat(dayWTH, 64)
+			var oned = onede / 100
+			if err3225 != nil {
+				fmt.Println(err3225)
+			}
+			week := arr[len(arr)-1]
+			weekWTH := strings.Replace(week, "%", "", -1)
+			var onewe, err1225 = strconv.ParseFloat(weekWTH, 64)
+			var onew = onewe / 100
+			if err1225 != nil {
+				fmt.Println(err1225)
+			}
+
+			InsertCurrency(Currency{currName, currSymbol, parsedMC, parsedPrice, parsupply, volS, oned, oneh, onew})
+
+		}
+		go func() {
+			wg1.Wait()
+			wg.Done()
+		}()
+
+	}
+	go func() {
+		wg.Wait()
+	}()
+	return true
+
 }
 
 func getUrls() {
@@ -176,9 +343,8 @@ func getUrls() {
 	}
 	json.Unmarshal(raw, &urlArr)
 
-	for result := range resultNodeExchangeGen(rootGen(respGen(urlArr.Urls...))) {
-		fmt.Println(result)
-	}
+	resultNodeExchangeGen(rootGen(respGen(urlArr.Urls...)))
+	// resultNodeCurrencyGen(rootGen(respGen("https://coinmarketcap.com/all/views/all/")))
 	// jsonData, err := json.Marshal(resultList)
 
 	if err != nil {
